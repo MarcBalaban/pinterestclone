c23cb93b703d9d09f3307cd9c767e9b03645b7e4
Marc Balaban <marc@marcbalaban.com>
2014-01-08 16:20:01 -0500

    add S3 paperclip for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ PinterestClone::Application.configure do
   # Note to set this to your actual host name.
   config.action_mailer.default_url_options = { :host => 'pinterest-light.herokuapp.com' }
 
+  # configuration for S3 Server for Paperclip and uploading images.
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
